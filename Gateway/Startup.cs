f880dcb2fbec6c3005a210fed64bec9e271eb363
Enes Aydos <aydosenes@outlook.com>
2022-08-27 20:05:24 +0300

    Add project files.

---
+++ Gateway/Startup.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gateway
+{
+    public class Startup
+    {
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot();
+        }
+
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            await app.UseOcelot();            
+        }
+    }
+}
---
+++ MessageBrokerService/Messages/CreateReportMessageCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageBrokerService.Messages
+{
+    public class CreateReportMessageCommand
+    {
+        public ReportDto Report { get; set; }
+    }
+
+    public class ReportDto
+    {
+        public string Location { get; set; }
+        public int ContactCount { get; set; }
+        public int PhoneCount { get; set; }
+        public byte State { get; set; }
+    }
+}
